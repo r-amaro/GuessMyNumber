a23a5ee4d62afa091a4d213791ef6610a1c88d52
Rafael Amaro <ralf.g.amaro@gmail.com>
2021-06-29 18:11:14 -0500

    First commit

---
+++ GuessMyNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            bool found = false;
+
+            Console.WriteLine("Guess my number!");
+            Console.WriteLine("Pick one between 1-100");
+            int number = rnd.Next(100);//sets the number as a random number between 1-100
+            while (!found)
+            {//while the number hasn't been found
+                int guess = Convert.ToInt32(Console.ReadLine());//allows the user to continue guessing
+                if (guess == number)
+                {
+                    Console.WriteLine("You guessed my number!");
+                    found = true;
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Guess higher!");
+                }
+                else
+                {
+                    Console.WriteLine("Guess lower!");
+                }
+            }
+            Console.ReadLine();//Lets the user read the victory message, press Enter to exit
+        }
+    }
+}
